224df464f55df52ae4174394da83bf6b8068f3d1
HOG <hogseruj1@gmail.com>
2025-11-28 21:07:35 +0200

    Added errors components.

--- app/notes/NoteDetails.client.tsx
+++ app/notes/NoteDetails.client.tsx
@@ -10,20 +10,32 @@ import { Note } from '@/types/note';
 export default function NoteDetailsClient() {
   const { id } = useParams<{ id: string }>();
 
-  const { data: note } = useQuery<Note>({
+  const {
+    data: note,
+    isLoading,
+    isError,
+  } = useQuery<Note>({
     queryKey: ['note', id],
     queryFn: () => fetchNoteById(id),
     refetchOnMount: false,
   });
 
+  if (isLoading) {
+    return <p>Loading, please wait...</p>;
+  }
+
+  if (isError || !note) {
+    return <p>Something went wrong.</p>;
+  }
+
   return (
     <div className={css.container}>
       <div className={css.item}>
         <div className={css.header}>
-          <h2>{note?.title}</h2>
+          <h2>{note.title}</h2>
         </div>
         <p className={css.content}>{note?.content}</p>
-        <p className={css.date}>{note?.updatedAt || note?.createdAt}</p>
+        <p className={css.date}>{note.updatedAt || note.createdAt}</p>
       </div>
     </div>
   );
